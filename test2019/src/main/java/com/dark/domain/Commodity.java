13987503ea3535c9be7b28b3366f01bcbf76e689
dark0eternity <dark_eternity@tom.com>
2019-09-23 23:12:22 +0800

    给bean添加无参与满参构造方法

--- test2019/src/main/java/com/dark/domain/Commodity.java
+++ test2019/src/main/java/com/dark/domain/Commodity.java
@@ -13,4 +13,15 @@ public class Commodity {
     private Double price;
     private Date birthday;
     private String describes;
+
+    public Commodity() {
+    }
+
+    public Commodity(Integer id, String name, Double price, Date birthday, String describes) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.birthday = birthday;
+        this.describes = describes;
+    }
 }
